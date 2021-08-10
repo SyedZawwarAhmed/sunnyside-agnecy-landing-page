769e0a3a55c71131bb967af8881f6f663c8f54b1
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-08-10 20:19:02 +0500

    Navbar added

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../stylesheets/Navbar.css"
+
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <img className="logo" src="../../assets/images/logo.svg" />
+        <ul className="links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a className="contact-btn" href="#contact">Contact</a></li>
+        </ul>
+        <img className="hamburger" src="../../assets/images/icon-hamburger.svg" />
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
