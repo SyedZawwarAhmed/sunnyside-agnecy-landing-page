171a82ab2673daa53a0451175778c263a62b1662
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-08-11 16:18:59 +0500

    home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
 import "../stylesheets/Home.css";
+import arrow from "../assets/images/icon-arrow-down.svg";
 
 function Home() {
   return (
     <>
       <div className="home">
         <Navbar />
-        <h1 className="title">We are Creatives</h1>
-        <img src="../assets/images/icon-arrow-down.svg" alt="" />
+        <div className="title-div">
+          <h1 className="title">We are Creatives</h1>
+          <img className="arrow-down" src={arrow} alt="" />
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,30 @@
 import React from "react";
-import "../stylesheets/Navbar.css"
+import "../stylesheets/Navbar.css";
+import hamburger from "../assets/images/icon-hamburger.svg";
+import logo from "../assets/images/logo.svg";
 
 function Navbar() {
   return (
     <>
       <div className="navbar">
-        <img className="logo" src="../assets/images/logo.svg" />
+        <img className="logo" src={logo} />
         <ul className="links">
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a className="contact-btn" href="#contact">Contact</a></li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a className="contact-btn" href="#contact">
+              Contact
+            </a>
+          </li>
         </ul>
-        <img className="hamburger" src="../assets/images/icon-hamburger.svg" />
+        <img className="hamburger" src={hamburger} />
       </div>
     </>
   );
